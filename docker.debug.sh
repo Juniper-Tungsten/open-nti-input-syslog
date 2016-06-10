18ff84c927137384a6c4f22ab2e933b8b14f78dc
Damien <dgarros@gmail.com>
2016-06-10 10:40:16 -0700

    Update debug script

--- docker.debug.sh
+++ docker.debug.sh
@@ -1,15 +1,16 @@
 #! /bin/bash
 
 docker run --rm -t \
-        -e OUTPUT_KAFKA="true" \
-        -e OUTPUT_INFLUXDB="false" \
-        -e INFLUXDB_ADDR="172.17.0.3" \
+        -e OUTPUT_KAFKA="false" \
+        -e OUTPUT_INFLUXDB="true" \
+        -e INFLUXDB_ADDR="172.29.110.16" \
         -e INFLUXDB_NUM_THREADS="3" \
         -e INFLUXDB_FLUSH_INTERVAL="5s" \
-        -e KAFKA_ADDR="10.0.0.121" \
+        -e KAFKA_ADDR="172.29.110.16" \
         -e KAFKA_TOPIC="events" \
         -e OUTPUT_STDOUT="false" \
         -e PORT_SYSLOG='16000' \
         -p 16000:16000/udp \
         -p 24220:24220 \
-        -i juniper/open-nti-input-syslog /bin/sh
+        -v $(pwd)/fluent.conf:/fluentd/etc/fluent.conf \
+        -i juniper/open-nti-input-syslog 
